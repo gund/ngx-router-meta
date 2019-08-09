ce025aca4ddee6df9212fa08ba46cafcfa8ef7c3
Alex Malkevich <alex.malkevich@mcmakler.de>
2019-08-09 13:24:42 +0200

    docs(demo-ssr): add server side rendering

--- projects/demo-ssr/src/app/app.module.ts
+++ projects/demo-ssr/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
