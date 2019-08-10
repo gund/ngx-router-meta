c73e5a7118a360474d45c8c51b316418732a55cc
gund <malkevich.alex@gmail.com>
2019-08-10 02:30:26 +0200

    docs(demo-ssr): add TransferState related modules to app

--- projects/demo-ssr/src/app/app.module.ts
+++ projects/demo-ssr/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  BrowserTransferStateModule,
+} from '@angular/platform-browser';
 import { RouterMetaModule } from 'ngx-router-meta';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +11,8 @@ import { ShowRouteComponent } from './show-route.component';
 
 @NgModule({
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: 'demo-ssr' }),
+    BrowserTransferStateModule,
     RouterMetaModule.forRoot(),
     AppRoutingModule,
   ],
