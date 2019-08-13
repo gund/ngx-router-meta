07774ea4facfcbf73fc1b44f344796e49af8c328
gund <malkevich.alex@gmail.com>
2019-08-13 22:40:14 +0200

    fix(lib): adjust public API to export only public entities

--- projects/ngx-router-meta/src/public-api.ts
+++ projects/ngx-router-meta/src/public-api.ts
@@ -3,6 +3,6 @@
  */
 
 export * from './lib/router-meta';
-export * from './lib/router-meta.service';
-export * from './lib/router-meta-context.service';
+export { RouterMetaService } from './lib/router-meta.service';
+export { RouterMetaContextService } from './lib/router-meta-context.service';
 export * from './lib/router-meta.module';
